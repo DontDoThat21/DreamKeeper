2ced77747fbe338f4db55c516390358cbf876a20
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-05-04 11:36:50 -0400

    Add project files.

---
+++ DreamKeeper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DreamKeeper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DreamKeeper/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DreamKeeper
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
