d6aee3309e07b5cb6e10767594c32b40238cd904
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-23 14:17:27 -0400

    Added Byte Array Media Element and Source with template selector for custom Media Element control.

---
+++ DreamKeeper/Data/ByteArrayMediaElement.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Maui.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamKeeper.Data
+{
+    public class ByteArrayMediaElement : MediaElement
+    {
+        public static readonly BindableProperty AudioDataProperty = BindableProperty.Create(
+            nameof(AudioData), typeof(byte[]), typeof(ByteArrayMediaElement), default(byte[]), propertyChanged: OnAudioDataChanged);
+
+        public byte[] AudioData
+        {
+            get => (byte[])GetValue(AudioDataProperty);
+            set => SetValue(AudioDataProperty, value);
+        }
+
+        private static void OnAudioDataChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (ByteArrayMediaElement)bindable;
+            control.UpdateMediaSource();
+        }
+
+        private void UpdateMediaSource()
+        {
+            if (AudioData != null && AudioData.Length > 0)
+            {
+                var tempFilePath = Path.Combine(FileSystem.CacheDirectory, "tempRecording.m4a");
+                // Write byte array to a temporary file
+                File.WriteAllBytes(tempFilePath, AudioData);
+                // Set the source to the temporary file
+                this.Source = MediaSource.FromFile(tempFilePath);
+            }
+        }
+    }
+}
---
+++ DreamKeeper/Data/ByteArrayMediaSource.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace DreamKeeper.Data
+{
+    public class ByteArrayMediaSource : MediaSource
+    {
+        public byte[] Data { get; set; }
+
+        public ByteArrayMediaSource(byte[] data)
+        {
+            Data = data;
+        }
+    }
+}
